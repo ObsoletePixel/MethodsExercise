a9149ed699d6059dd70db14490e1356633695b56
ObsoletePixel <obsoletepixel@gmail.com>
2025-03-25 16:04:23 -0700

    First commit for the MethodsExercise project

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace MethodsExercise
+﻿namespace MethodsExercise 
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var yourName = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("Where are you from?");
+            var hometown = Console.ReadLine();
+
+            IntroGen(yourName, age, hometown);
         }
+
+        static void IntroGen(string yourName, string age, string hometown)
+        {
+            Console.WriteLine($"Hello! My name is {yourName}, I am {age}, and I am from {hometown}. It's nice to meet you!");
+        }
+        
     }
-}
+};
